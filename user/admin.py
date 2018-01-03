96ceecafaf6a01b35bf61e45e19b38c4639552e0
psdfeng <psdfeng@live.com>
2018-01-03 20:14:14 +0800

    models

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import User, Rank, Group, Type, Dept
+
+
 # Register your models here.
+
+class RankInline(admin.TabularInline):
+    model = Rank
+
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('工号', {'fields': ['userID']}),
+        ('姓名', {'fields': ['userName']}),
+        ('密码', {'fields': ['userPassword']}),
+        ('开始时间', {'fields': ['userStDatetime']}),
+        ('结束时间', {'fields': ['userEndDatetime']}),
+    ]
+    list_display = ('userID', 'userName', 'userName', 'userPassword', 'userStDatetime', 'userEndDatetime', 'was_user_start_not_end')
+    list_filter = ['userStDatetime']
+    search_fields = ['userID', 'userName']
+
+
+admin.site.register(User, UserAdmin)
